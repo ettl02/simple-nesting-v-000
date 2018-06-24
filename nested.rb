39d86c7234419b5b595698053dc56ba4e508f354
Emily Wessel <ettl02@gmail.com>
2018-06-24 21:07:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -29,12 +29,12 @@ def alan_kay_is_known_for
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C" "Assembly"]
         }
      }
 programmer_hash[:alan_kay][:known_for]
